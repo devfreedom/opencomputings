56f91129c21d3da387e6c6f7ba5acb111bfb339c
owner <devfreedomHQ@protonmail.com>
2022-04-06 18:14:28 +0900

    Add hero section

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+
+    extend: {
+      fontFamily: {
+        'fira-code' : 'Fira Code, sans-serif',
+        'fira-sans' : 'Fira Sans, sans-serif',
+        'lato' : 'Lato, sans-serif',
+        'open sans' : 'Open Sans, sans-serif',
+        'source-sans-pro' : 'Source Sans Pro, sans-serif',
+        'ubuntu' : 'Ubuntu, sans-serif'
+      }
+    },
+
+  },
+  plugins: [],
+}
