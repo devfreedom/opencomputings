a1da72ae418d8c4b350ea28fa9cb21355c2ae5bc
owner <devfreedomHQ@protonmail.com>
2022-04-08 21:05:48 +0900

    Add initial Javascript file

---
+++ index.js
@@ -0,0 +1,21 @@
+var menuVisibility = document.getElementById("header-menu-mobile");
+var menuOpenBtnVisibility = document.getElementById("menu-open-btn");
+var menuCloseBtnVisibility = document.getElementById("menu-close-btn");
+
+function openMenu() {
+    menuVisibility.removeAttribute('class');
+    menuVisibility.setAttribute('class', 'z-20 absolute flex laptop:hidden w-full h-[50vh] bg-white drop-shadow-xl flex flex-col justify-center items-center justify-around py-10');
+    menuOpenBtnVisibility.removeAttribute('class');
+    menuOpenBtnVisibility.setAttribute('class', 'hidden laptop:hidden justify-center space-y-1.5 mx-2');
+    menuCloseBtnVisibility.removeAttribute('class');
+    menuCloseBtnVisibility.setAttribute('class', 'flex laptop:hidden justify-center items-center mx-2');
+}
+
+function closeMenu() {
+    menuVisibility.removeAttribute('class');
+    menuVisibility.setAttribute('class', 'z-20 absolute hidden laptop:hidden w-full h-[50vh] bg-white drop-shadow-xl flex flex-col justify-center items-center justify-around py-10');
+    menuOpenBtnVisibility.removeAttribute('class');
+    menuOpenBtnVisibility.setAttribute('class', 'flex flex-col laptop:hidden justify-center space-y-1.5 mx-2');
+    menuCloseBtnVisibility.removeAttribute('class');
+    menuCloseBtnVisibility.setAttribute('class', 'hidden justify-center items-center mx-2');
+}
